8b5e48b2a416e04e070ef1fe7803fe1cafc456c2
wanderley Landim <wanderleylandim@wanderley.local>
2018-04-10 13:56:38 -0300

    first commit

---
+++ spec/crud.rb
@@ -0,0 +1,61 @@
+
+require 'httparty'
+require 'spec_helper'
+
+class Api
+  include HTTParty
+  base_uri 'http://localhost:3000/'
+end
+
+RSpec.describe 'info json server - requisição GET' do
+  it 'efetuando requisição GET' do
+    @response = Api.get('/users')
+    expect(@response.code).to eql(200)
+    expect(@response.message).to eql('OK')
+  end
+end
+
+RSpec.describe 'info json server - requisição POST' do
+  it 'efetuando requisição POST' do
+         @usuario = 
+          {
+            "name": "dureza",
+            "location": "sp"
+          }  
+        
+
+    @response = Api.post('/users', :body => @usuario) 
+    expect(@response.code).to eql(201)
+    expect(@response.message).to eql('Created')
+  end
+end
+
+
+RSpec.describe 'info json server - requisição PUT' do
+  it 'efetuando requisição PUT' do
+         @usuario = 
+          {
+            "name": "dureza Andre",
+            "location": "sp"
+          }  
+        
+
+    @response = Api.put('/users/2', :body => @usuario)
+    expect(@response.code).to eql(200)
+  end
+end
+
+
+RSpec.describe 'info json server - requisição DELETE' do
+  it 'efetuando requisição DELETE' do
+    @response = Api.delete('/users/2', :body => @usuario)
+    expect(@response.code).to eql(200)
+  end
+end
+
+RSpec.describe 'info json server - requisição GET' do
+  it 'efetuando requisição GET no link que não existe' do
+    @response = Api.get('/users/TESTE')
+    expect(@response.code).to eql(404)
+  end
+end
